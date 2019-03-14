3bb0ab0365f07f46a4246484ec5945bebb553502
Jiarong Gu <jiaronggu@hotmail.com>
2019-03-15 01:22:18 +1100

    fix a few bugs of new service

--- src/middlewares/triggerMiddleware.ts
+++ src/middlewares/triggerMiddleware.ts
@@ -2,7 +2,7 @@ import { MiddlewareAPI, Dispatch, AnyAction } from 'redux';
 import { TriggerEvent, Function, Action } from '../types';
 
 const triggerEvents = new Map<string, Array<Function>>();
-const retriggerActions: { [key: string]: Action | null } = {};
+const retriggerActions: { [key: string]: any } = {};
 
 export const triggerMiddleware = (store: MiddlewareAPI<any>) => (next: Dispatch<AnyAction>) => (action: Action) => {
   if (retriggerActions[action.type] !== undefined) {
@@ -15,9 +15,8 @@ export const triggerMiddleware = (store: MiddlewareAPI<any>) => (next: Dispatch<
 // process location task, return task array
 export async function runTriggerEvents(action: Action) {
   const triggers = triggerEvents.get(action.type);
-  const payload = Array.isArray(action.payload) ? action.payload : [ action.payload ];
   if (triggers)
-    await Promise.all(triggers.map(handler => handler(...payload)));
+    await Promise.all(triggers.map(handler => handler(...formatPayload(action.payload))));
 }
 
 // add new location event and run location tasks after
@@ -34,10 +33,14 @@ export function registerTrigger(handler: TriggerEvent) {
   triggers.sort((a, b) => (b.priority || 0) - (a.priority || 0));
 
   if (retriggerActions[action] !== undefined) {
-    process(retriggerActions[action]);
+    process(...formatPayload(retriggerActions[action]));
   }
 }
 
-export function applyRetriggerAction(action: string, value: any = null) {
-  retriggerActions[action] = value;
+function formatPayload(payload: any) {
+  return Array.isArray(payload) ? payload : [ payload ];
+}
+
+export function applyRetriggerAction(action: string, payload: any = null) {
+  retriggerActions[action] = payload;
 }
