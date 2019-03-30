b887a15c30c91559542d85e5f2d88fb4c37e278b
Jiarong Gu <jiaronggu@hotmail.com>
2019-03-30 17:50:01 +1100

    remove unused types

--- src/typings.ts
+++ src/typings.ts
@@ -1,19 +1,13 @@
-import { Middleware, Store } from 'redux';
-import { SinkBuilder } from './SinkBuilder';
+import { Middleware } from 'redux';
 
 export type Constructor<T = any, A = any> = { new(...args: Array<A>): T };
 
-// action / reducer
 export interface Action<Payload = any> {
   type: string;
   payload?: Payload;
 }
-export type Reducer<TState, TPayload> = (state: TState, action: Action<TPayload>) => TState;
 
-// functions
-export type Function = (...args: Array<any>) => any;
 export type PayloadHandler<TPayload = any> = (payload: TPayload) => any;
-export type ActionFunction = (...args: Array<any>) => Action;
 
 export interface TriggerOptions {
   priority?: number;
