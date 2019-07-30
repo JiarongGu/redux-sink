74e2700c36b82a4c64ab38e2257cedd64196e1c2
Jiarong Gu <jiaronggu@hotmail.com>
2019-07-30 12:31:17 +0900

    fix mapdistachstate without state

--- src/utilities/mergeDispatchState.ts
+++ src/utilities/mergeDispatchState.ts
@@ -2,14 +2,16 @@ import { SinkDispatch } from '../typings';
 import { reduceKeys } from './reduceKeys';
 
 export function mergeDispatchState<T>(dispatches: { [key: string]: SinkDispatch }, state: any) {
-  const defined = reduceKeys(Object.keys(state), (key) => {
-    return {
-      get: () => state[key],
-      set: (value: any) => dispatches[key](value),
-    };
-  });
   const merged = Object.assign({}, dispatches);
-  Object.defineProperties(merged, defined);
 
+  if (state) {
+    const defined = reduceKeys(Object.keys(state), (key) => {
+      return {
+        get: () => state[key],
+        set: (value: any) => dispatches[key](value),
+      };
+    });
+    Object.defineProperties(merged, defined);
+  }
   return merged as any as T;
 }
